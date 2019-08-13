9ce89e8f909e786513ced39dbc574d57a20461fa
TrevorNewey <31452349+TrevorNewey@users.noreply.github.com>
2019-08-12 21:03:04 -0600

    Search returning data

---
+++ WeatherForecast/Hubs/WeatherHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+using WeatherForecast.HttpClientService;
+
+namespace WeatherForecast.Hubs
+{
+    public class WeatherHub : Hub
+    {
+        public async Task SendCoordinates(string lat, string lng)
+        {
+            WeatherService weatherService = new WeatherService();
+            double dlat = 0.0;
+            double dlng = 0.0;
+            dlat = Convert.ToDouble(lat);
+            dlng = Convert.ToDouble(lng);
+
+            string result = await weatherService.GetCurrentWeatherAsync(dlng,dlat);
+            await Clients.All.SendAsync("ReceiveResult", result);
+        }
+    }
+}
